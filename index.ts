96ae38e7bfd456c7e4ccefb022d35ff757b81d2e
AyeshaParwaiz <164402044+AyeshaParwaiz@users.noreply.github.com>
2024-04-16 20:45:33 +0500

    created todo-list

---
+++ index.ts
@@ -0,0 +1,29 @@
+#!usr/bin/env node
+
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+
+while(condition)
+    {
+        let task = await inquirer.prompt(
+            [
+                {
+                    name : "todo",
+                    type : "input",
+                    message : "What you want to add in your Todos?"
+                },
+
+                {
+                    name : "addMore",
+                    type : "confirm",
+                    message : "Do you want to add more?",
+                    default : "false"
+                }
+            ]
+        );
+
+        todos.push(task.todo);
+        condition = task.addMore;
+        console.log(todos)
+        }
